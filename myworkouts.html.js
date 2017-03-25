9c85f365dafce01d7304a416fdbc3943777cec34
Matt McNeil <hmqcnoesy@gmail.com>
2017-03-25 12:54:49 -0600

    git sucks

--- myworkouts.html.js
+++ myworkouts.html.js
@@ -15,13 +15,48 @@
         }        
     });
 
+
     document.getElementById('divMyWorkouts').addEventListener('click', function(e) {
-        if (e.target.tagName != 'A') return;
-        var div = e.target.parentNode.querySelector('div');
+        if (e.target.hasAttribute('data-toggle')) toggleVisibility(e.target);
+
+        var svg;
+
+        if (e.target.tagName.toLowerCase() == 'path') 
+            svg = e.target.parentNode;
+        else
+            svg = e.target;
+
+        var workoutName = svg.parentNode.parentNode.parentNode.querySelector('a').getAttribute('data-name');
+        if (svg.hasAttribute('data-edit')) editWorkout(workoutName);
+        if (svg.hasAttribute('data-delete')) deleteWorkout(workoutName);
+    }, true);
+
+
+    function drawWorkout(targetElement, workout) {
+        var w = new Workout();
+        var settings = {
+            horizSecondsPerPixel: 20, 
+            verticalPercentsPerPixel: 3, 
+            shapeHeight: 60,
+            showCadenceIndicator: false,
+            showTextEventIndicator: false,
+            minShapeWidth: 3 };
+        w.reconstituteFromDeserialized(workout);
+        for (var i = 0; i < w.segments.length; i++) {
+            var svgs = w.segments[i].toSvgs(settings);
+            for (var j = 0; j < svgs.length; j++) {
+                targetElement.appendChild(svgs[j]);
+            }
+        }
+    }
+
+
+    function toggleVisibility(aElement) {
+        var div = aElement.parentNode.querySelector('div');
         var invisible = div.classList.contains('invisible');
 
         if (invisible) {
-            var workout = userSettings.getMyWorkout(e.target.getAttribute('data-name'));
+            var workout = userSettings.getMyWorkout(aElement.getAttribute('data-name'));
             if (!workout) return;
             div.querySelector('[data-tags]').innerHTML = '';
             div.querySelector('[data-tags]').appendChild(document.createTextNode(workout.tags));
@@ -35,23 +70,15 @@
         } else {
             div.classList.add('invisible');
         }
-    });
+    }
 
-    function drawWorkout(targetElement, workout) {
-        var w = new Workout();
-        var settings = {
-            horizSecondsPerPixel: 20, 
-            verticalPercentsPerPixel: 3, 
-            shapeHeight: 75,
-            showCadenceIndicator: false,
-            showTextEventIndicator: false,
-            minShapeWidth: 3 };
-        w.reconstituteFromDeserialized(workout);
-        for (var i = 0; i < w.segments.length; i++) {
-            var svgs = w.segments[i].toSvgs(settings);
-            for (var j = 0; j < svgs.length; j++) {
-                targetElement.appendChild(svgs[j]);
-            }
-        }
+
+    function editWorkout(workoutName) {
+        console.log('editing ' + workoutName);
+    }
+
+
+    function deleteWorkout(workoutName) {
+        console.log(workoutName + ' deleted');
     }
 })();
