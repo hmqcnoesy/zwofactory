7d4bbbba86b0a77ce75eb2c809c9a1885f41f5df
Matt McNeil <hmqcnoesy@gmail.com>
2017-03-29 18:44:03 -0600

    git sucks

---
+++ convert/convert.js
@@ -0,0 +1,13 @@
+var fs = require('fs');
+var path = require('path');
+var DOMParser = require('xmldom').DOMParser;
+
+var filedata = fs.readFileSync('../zwofactory.js', 'utf8');
+eval(filedata);
+var xml = fs.readFileSync(process.argv[2], 'utf8');
+var workout = new Workout();
+workout.loadFromXml(xml);
+for(var i = 0; i < workout.segments.length; i++) {
+    delete workout.segments[i].id;
+}
+console.log(JSON.stringify(workout));
---
+++ convert/convertall.ps1
@@ -0,0 +1,19 @@
+$jsonFile = 'workouts.json'
+
+New-Item $jsonFile -type file -force -value "["
+
+$pwd = pwd
+$pwd = $pwd.path
+$zwos = ls *.zwo -R 
+
+
+
+foreach($zwo in $zwos) {
+    $cmd = "node convert.js """ + $zwo.fullname.replace($pwd + "\", "") + """"
+    $zwo.fullname.replace($pwd + "\", "")
+    $jsonData = iex $cmd 
+    $jsonData = $jsonData.replace("{""name"":", "{""path"":""" + $zwo.fullname.replace($pwd + "\workouts\", "").replace("\" + $zwo.name, "") + """,""name"":")
+    $jsonData | add-content $jsonFile
+}
+
+add-content $jsonFile "]"
